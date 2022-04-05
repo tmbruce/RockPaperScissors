4d1f5623e3d027ad24e378fba8c36ff9c43394e5
Travis <travis.m.bruce@gmail.com>
2022-04-04 17:19:34 -0700

    Complete gameplay with ui

--- script.js
+++ script.js
@@ -1,86 +1,97 @@
+let playerScore = document.querySelector('#human-score');
+let computerScore = document.querySelector('#computer-score');
+let outputMessage = document.querySelector('.message');
+
+let rockButton = document.querySelector('.rock');
+let paperButton = document.querySelector('.paper');
+let scissorButton = document.querySelector('.scissors');
+
+let gameRound = 0;
+
 let scoreBoard = { player: 0, computer: 0 };
-      const options = ["Rock", "Paper", "Scissors"];
-      const playerSelection = () => {
-        let options = "Rock, Paper, or Scissors?";
-        let choice = prompt(options);
-        return choice;
-      };
 
-      const computerPlay = () => {
-        let randomNum = Math.floor(Math.random() * 3);
-        return options[randomNum];
-      };
-      const capitalize = (word) => word.charAt(0).toUpperCase() + word.slice(1);
+const options = ['rock', 'paper', 'scissors'];
 
-      const playRound = (playerSelection, computerSelection) => {
-        playerSelection = playerSelection.toLowerCase();
-        computerSelection = computerSelection.toLowerCase();
-        let output = "";
-        if (playerSelection == computerSelection) {
-          output = "Tie! Try again!";
-        } else {
-          if (playerSelection == "rock") {
-            if (computerSelection == "paper") {
-              output = `You lose! ${capitalize(
-                computerSelection
-              )} beats ${capitalize(playerSelection)}`;
-              scoreBoard.computer += 1;
-            } else {
-              output = `You Win! ${capitalize(
-                playerSelection
-              )} beats ${capitalize(computerSelection)}`;
-              scoreBoard.player += 1;
-            }
-          }
-          if (playerSelection == "paper") {
-            if (computerSelection == "scissors") {
-              output = `You lose! ${capitalize(
-                computerSelection
-              )} beats ${capitalize(playerSelection)}`;
-              scoreBoard.computer += 1;
-            } else {
-              output = `You Win! ${capitalize(
-                playerSelection
-              )} beats ${capitalize(computerSelection)}`;
-              scoreBoard.player += 1;
-            }
-          }
-          if (playerSelection == "scissors") {
-            if (computerSelection == "rock") {
-              output = `You lose! ${capitalize(
-                computerSelection
-              )} beats ${capitalize(playerSelection)}`;
-              scoreBoard.computer += 1;
-            } else {
-              output = `You Win! ${capitalize(
-                playerSelection
-              )} beats ${capitalize(computerSelection)}`;
-              scoreBoard.player += 1;
-            }
-          }
-        }
-        console.log(scoreBoard);
-        return output;
-      };
+rockButton.addEventListener('click', () => {
+  playGame(options[0]);
+});
 
-      const game = (rounds) => {
-        let output = "";
-        for (let i = 0; i < rounds; i++) {
-          if (
-            scoreBoard.player > Math.floor(rounds / 2) ||
-            scoreBoard.player > Math.floor(rounds / 2)
-          ) {
-            i = rounds;
-          } else {
-            playRound(playerSelection(), computerPlay());
-          }
-        }
-        if (scoreBoard.computer > scoreBoard.player) {
-          output = `You Loose! Computer: ${scoreBoard.computer} You: ${scoreBoard.player}}`;
-        } else {
-          output = `You Won! Your score: ${scoreBoard.player} Computer score: ${scoreBoard.computer}`;
-        }
+paperButton.addEventListener('click', () => {
+  playGame(options[1]);
+});
 
-        return output;
-      };
-      //console.log(game(5));
+scissorButton.addEventListener('click', () => {
+  playGame(options[2]);
+})
+
+const capitalize = (word) => word.charAt(0).toUpperCase() + word.slice(1);
+
+const computerPlay = () => {
+  let randomNum = Math.floor(Math.random() * 3);
+  return options[randomNum];
+};
+
+const playRound = (playerSelection, computerSelection) => {
+  let output = "";
+  if (playerSelection == computerSelection) {
+    output = "Tie! Try again!";
+  } else {
+    if (playerSelection == "rock") {
+      if (computerSelection == "paper") {
+        output = `You lose! ${capitalize(
+          computerSelection
+        )} beats ${capitalize(playerSelection)}`;
+        scoreBoard.computer += 1;
+      } else {
+        output = `You Win! ${capitalize(
+          playerSelection
+        )} beats ${capitalize(computerSelection)}`;
+        scoreBoard.player += 1;
+      }
+    }
+    if (playerSelection == "paper") {
+      if (computerSelection == "scissors") {
+        output = `You lose! ${capitalize(
+          computerSelection
+        )} beats ${capitalize(playerSelection)}`;
+        scoreBoard.computer += 1;
+      } else {
+        output = `You Win! ${capitalize(
+          playerSelection
+        )} beats ${capitalize(computerSelection)}`;
+        scoreBoard.player += 1;
+      }
+    }
+    if (playerSelection == "scissors") {
+      if (computerSelection == "rock") {
+        output = `You lose! ${capitalize(
+          computerSelection
+        )} beats ${capitalize(playerSelection)}`;
+        scoreBoard.computer += 1;
+      } else {
+        output = `You Win! ${capitalize(
+          playerSelection
+        )} beats ${capitalize(computerSelection)}`;
+        scoreBoard.player += 1;
+      }
+    }
+  }
+  playerScore.textContent = scoreBoard.player;
+  computerScore.textContent = scoreBoard.computer;
+  outputMessage.textContent = output;
+};
+
+const playGame = (playerSelection) => {
+  playRound(playerSelection, computerPlay());
+  gameRound = Math.max(scoreBoard.player, scoreBoard.computer);
+
+  if (gameRound > 4) {
+    if(scoreBoard.player > scoreBoard.computer) {
+      outputMessage.textContent = 'Congratulations, you won the game! Make a new selection to play again!';
+    } else {
+      outputMessage.textContent = 'Bummer! You lost! Make a new selection to play again!'
+    }
+    scoreBoard.player = 0;
+    scoreBoard.computer = 0;
+  }
+}
